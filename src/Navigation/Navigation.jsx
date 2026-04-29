ea1482553217068ee39c73526c7ad002abdcb131
Sumally007 <ismailmbarouk007@gmail.com>
2026-04-29 15:23:06 +0300

    setting up the project and building navigation

---
+++ src/Navigation/Navigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "../../src/Navigation/Navigation.css";
+const Navigation = () => {
+    return (
+        <nav className='navigation'>
+            <div className='navigation-container'>
+                <div className='logo-container'>
+                    <img src="" alt="" />
+                    <span>Y-Ya</span>
+                </div>
+                <div className='lc'>
+                    <ul className="list-items">
+                        <li>Features</li>
+                        <li>How It Works</li>
+                        <li>FAQ</li>
+                    </ul>
+                </div>
+                <div className='btn-container'>
+                    <button>Wire an Event</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation
