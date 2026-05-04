22f25c076d4d476366f141bd81dd0ece706e9dbd
Sumally007 <ismailmbarouk007@gmail.com>
2026-05-04 14:51:58 +0300

    styling accordion section

--- src/Accordion/Accordion.jsx
+++ src/Accordion/Accordion.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
+import "../../src/Accordion/Accordion.css";
 import { AccordionData } from './AccordionData'
+import { MdOutlineKeyboardArrowUp, MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 const Accordion = () => {
     return (
         <section>
             <div className='accordion'>
+                <h1>Frequently Asked Questions</h1>
+                <p>These are the most common questions about Y-Ya.</p>
+                <p>Can't find what you're looking for? <span>Chat to our friendly team!</span></p>
                 {AccordionData.map(({ title, content, icon, id }) => (
                     <AccordionComponet title={title} content={content} id={id} icon={icon} />
                 ))}
@@ -21,7 +26,7 @@ const AccordionComponet = ({ title, content, icon, id }) => {
         <div className="header-accordion" onClick={() => setIsActive(!isActive)}>
             <div><Icon /></div>
             <div>{title}</div>
-            <p className='icon-toogle'>{isActive ? "v" : "^"}</p>
+            <p className='icon-toogle'>{isActive ? <MdOutlineKeyboardArrowDown /> : <MdOutlineKeyboardArrowUp />}</p>
         </div>
 
         <div className="content-accordion">
