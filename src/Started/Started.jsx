84a0a38dde88240efc2cd3f7809c48b6b2ddc88e
Sumally007 <ismailmbarouk007@gmail.com>
2026-05-10 16:47:04 +0300

    building left started content

--- src/Started/Started.jsx
+++ src/Started/Started.jsx
@@ -1,14 +1,54 @@
 import React from 'react'
+import { LuArrowUpRight, LuQrCode } from "react-icons/lu";
+import { CiGift, CiCamera } from "react-icons/ci";
+import "../../src/Started/Started.css";
+
+
 
 const Started = () => {
     return (
         <section className='started-section'>
             <div className="started-container">
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className='left-started-content'>
+                    <p className='top-header'>Let's get started</p>
+                    <h1>Make Your Next Event <span>Unforgettable</span></h1>
+                    <p className='sub-intro'>Talk to us early so your event can be set up and ready before the big day.</p>
+                    <button className='btn'>Wire an Event <LuArrowUpRight /></button>
+                </div>
+                <div className='middile-started-content'>
+                    <div className="icon-box">
+                        <LuQrCode className='icon' />
+                    </div>
+                    <div>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div className="icon-box">
+                        <CiGift className='icon' />
+                    </div>
+                    <div className="icon-box">
+                        <CiCamera className='icon' />
+                    </div>
+                </div>
+                <div className='right-started-content'>
+                    <div className="circle-container">
+                        <div className='profile-picture'>AK</div>
+                        <div className='profile-picture'>BM</div>
+                        <div className='profile-picture'>CD</div>
+                        <div className='profile-picture'>EN</div>
+                        <div className='profile-picture'>+500</div>
+                    </div>
+                    <h3>Event organizers already trust Y-Ya. Join them!</h3>
+                    <p>Perfect for weddings, send-offs, celebrations, and fundraisers. One QR code for everything.</p>
+                    <div className="buttons-container">
+                        <button>WhatsApp</button>
+                        <button>Instagram</button>
+                        <button>TikTok</button>
+                    </div>
+                </div>
             </div>
-        </section>
+        </section >
     )
 }
 
