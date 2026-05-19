34d46ab84e503c7fc28d0f9c47c05ae48510175e
Sumally007 <ismailmbarouk007@gmail.com>
2026-05-19 12:52:41 +0300

    building mobile nav

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -7,6 +7,25 @@ import { ListData, ListCompany, ListConnect } from '../List/ListData';
 const Navigation = () => {
     return (
         <nav className='navigation'>
+            <div className="mobile-nav">
+                <div className='top-nav'>
+                    <span>Y-Ya</span>
+                    <div className='nav-bar'>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+                <div className='menu-container'>
+                    <ul>
+                        {ListData.map((item) => (
+                            <List key={item.id} item={item} />
+                        ))}
+                    </ul>
+                    <button>Wire an Event</button>
+                </div>
+            </div>
+
             <div className='navigation-container'>
                 <div className='logo-container'>
                     {/* <img src="" alt="" /> */}
