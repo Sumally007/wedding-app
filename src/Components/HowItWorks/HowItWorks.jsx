13b207aecdb7511faae9c4460a0d7c924e0b032e
Sumally007 <ismailmbarouk007@gmail.com>
2026-06-07 07:28:58 +0300

    more factorization

--- src/Components/HowItWorks/HowItWorks.jsx
+++ src/Components/HowItWorks/HowItWorks.jsx
@@ -14,9 +14,9 @@ import { H2, H4, ParagraphNormal, ParagraphSubtitle, ParagraphSmall } from "../T
 const HowItWorks = () => {
     const cardData = [
         { id: 1, heading: "Contact Y-Ya", comment: "Reach out to wire your event and tell us what kind of celebration you are planning.", icon: <IoCallOutline className='icon' />, color: "lab(9 -0.81 -5.67)" },
-        { id: 2, heading: "Share Event Details", comment: "Provide event name, date, time, image, contact number, and your temporary lipa number.", icon: <GrDocumentText className='icon icon-active' />, color: "lab(57 3.31 -65.44)" },
-        { id: 3, heading: "We Set Everything Up", comment: "Y-Ya creates your event page, QR code, and the features included in your package.", icon: <IoSettingsOutline className='icon' /> },
-        { id: 4, heading: "Guests Scan & Celebrate", comment: "Guests scan the QR code to send gifts, upload moments, and join the live celebration.", icon: <LuScanLine className='icon' /> }
+        { id: 2, heading: "Share Event Details", comment: "Provide event name, date, time, image, contact number, and your temporary lipa number.", icon: <GrDocumentText className='icon icon-active' />, color: "lab(9 -0.81 -5.67)" },
+        { id: 3, heading: "We Set Everything Up", comment: "Y-Ya creates your event page, QR code, and the features included in your package.", icon: <IoSettingsOutline className='icon' />, color: "lab(9 -0.81 -5.67)" },
+        { id: 4, heading: "Guests Scan & Celebrate", comment: "Guests scan the QR code to send gifts, upload moments, and join the live celebration.", icon: <LuScanLine className='icon' />, color: "lab(9 -0.81 -5.67)" }
     ]
     return (
         <section className='how-it-works-section'>
@@ -57,6 +57,7 @@ const HowItWorks = () => {
 }
 
 const CardComponent = ({ item }) => {
+
     return (
         <div className="card" style={{ backgroundColor: item.color }}>
             <div className="icon-box">
@@ -68,7 +69,7 @@ const CardComponent = ({ item }) => {
                 <ParagraphNormal>{item.comment}</ParagraphNormal>
                 {/* <p>{item.comment}</p> */}
             </div>
-        </div>
+        </div >
     )
 }
 
